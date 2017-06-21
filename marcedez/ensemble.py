dc422f9a1f0bec652c50d9c5813c3ed687f259a1
Goman <linggom@ymail.com>
2017-06-21 21:01:24 +0700

    added feature'

--- marcedez/ensemble.py
+++ marcedez/ensemble.py
@@ -18,6 +18,9 @@ from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from bayes_opt import BayesianOptimization
+
+
 
 
 # In[ ]:
@@ -88,32 +91,30 @@ cellen=['ID', 'X47','X95','X314','X315','X232','X119','X311','X76','X329','X238'
 
 # In[ ]:
 
-from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
 
 rng = np.random.RandomState(42)
 
-print("starting remove outliers ")
-
-# Example settings
-n_samples = 200
-outliers_fraction = 0.25
-clusters_separation = [0, 1, 2]
-outl = IsolationForest(max_samples=n_samples,
-                                        contamination=outliers_fraction,
-                                        random_state=rng)
-outl.fit(train_feats_encode)
-
-
-# In[ ]:
-
-qw = outl.predict(train_feats_encode)
-np.unique(qw)
-
-
-# In[ ]:
-
-train_feats_encode = train_feats_encode[qw == 1]
-train_labels = train_labels[qw == 1]
+# print("starting remove outliers ")
+#
+# # Example settings
+# n_samples = 200
+# outliers_fraction = 0.25
+# clusters_separation = [0, 1, 2]
+# outl = OneClassSVM(random_state=rng)
+# outl.fit(train_feats_encode)
+#
+#
+# # In[ ]:
+#
+# qw = outl.predict(train_feats_encode)
+# np.unique(qw)
+#
+#
+# # In[ ]:
+#
+# train_feats_encode = train_feats_encode[qw == 1]
+# train_labels = train_labels[qw == 1]
 
 
 # In[ ]:
@@ -124,11 +125,17 @@ train_X, eval_X, train_y, eval_y = train_test_split(train_feats_encode, train_la
 # In[149]:
 print("starting modelling ")
 
-def check(model):
-    pred_train = model.predict(train_X)
-    score_train = r2_score(train_y, pred_train)
-    pred_eval = model.predict(eval_X)
-    score_eval = r2_score(eval_y, pred_eval)
+def check(model , exp=True, train_X=train_X, eval_X=eval_X, train_y=train_y, eval_y=eval_y):
+    if exp:
+        pred_train = np.exp(model.predict(train_X))
+        score_train = r2_score(train_y, pred_train)
+        pred_eval = np.exp(model.predict(eval_X))
+        score_eval = r2_score(eval_y, pred_eval)
+    else:
+        pred_train = model.predict(train_X)
+        score_train = r2_score(train_y, pred_train)
+        pred_eval = model.predict(eval_X)
+        score_eval = r2_score(eval_y, pred_eval)
     print(model.__class__.__name__)
     print("r2 train = ", score_train)
     print("r2 eval = ", score_eval)
@@ -139,31 +146,25 @@ def check(model):
 # In[150]:
 
 def eval_model():
-    print("TheilSenRegressor ")
+    # print("TheilSenRegressor ")
+    #
+    # model_theilsen = TheilSenRegressor(random_state=42)
+    # model_theilsen.fit(train_X, train_y)
+    # train_theilsen, test_theilsen, pred_train_theilsen, pred_eval_theilsen = check(model_theilsen)
+    #
 
-    model_theilsen = TheilSenRegressor(random_state=42)
-    model_theilsen.fit(train_X, train_y)
-    train_theilsen, test_theilsen, pred_train_theilsen, pred_eval_theilsen = check(model_theilsen)
-
+    y = np.log(train_y)
     print("RANSACRegressor ")
     model_ransac = RANSACRegressor(random_state=42)
-    model_ransac.fit(train_X, train_y)
+    model_ransac.fit(train_X, y)
     train_ransac, test_ransac, pred_train_ransac, pred_eval_ransac = check(model_ransac)
 
-    print("HuberRegressor ")
-    model_huber = HuberRegressor()
-    model_huber.fit(train_X, train_y)
-    train_huber, test_huber, pred_train_huber, pred_eval_huber = check(model_huber)
 
     print("LinearRegression ")
     model_linear = LinearRegression()
-    model_linear.fit(train_X, train_y)
+    model_linear.fit(train_X, y)
     train_lr, test_lr, pred_train_lr, pred_eval_lr = check(model_linear)
 
-    print("SGDRegressor ")
-    model_sgd = SGDRegressor()
-    model_sgd.fit(train_X, train_y)
-    train_sgd, test_sgd, pred_train_sgd, pred_eval_sgd = check(model_sgd)
 
     model_xgb = XGBRegressor(seed = 0,
       colsample_bytree = 0.7,
@@ -172,27 +173,27 @@ def eval_model():
       max_depth = 4,
       num_parallel_tree = 1,
       min_child_weight = 1, objective='reg:linear', base_score=np.mean(train_labels))
-    model_xgb.fit(train_X, train_y)
+    model_xgb.fit(train_X, y)
     train_xgb, test_xgb, pred_train_xgb, pred_eval_xgb = check(model_xgb)
 
     model_gbr = GradientBoostingRegressor()
-    model_gbr.fit(train_X, train_y)
+    model_gbr.fit(train_X, y)
     train_gbr, test_gbr, pred_train_gbr, pred_eval_gbr = check(model_gbr)
 
 
 
-    columns=['lr', 'sgd', 'xgb', 'gbr',  'thelisen', 'ransac', 'huber']
+    columns=['lr', 'xgb', 'gbr', 'ransac',]
 
-    test_res = (pred_eval_lr, pred_eval_sgd, pred_eval_xgb, pred_eval_gbr, pred_eval_theilsen, pred_eval_ransac, pred_eval_huber)
+    test_res = (pred_eval_lr, pred_eval_xgb, pred_eval_gbr, pred_eval_ransac)
 
-    train_res = (pred_train_lr, pred_train_sgd, pred_train_xgb, pred_train_gbr, pred_train_theilsen, pred_train_ransac, pred_train_huber)
+    train_res = (pred_train_lr, pred_train_xgb, pred_train_gbr, pred_train_ransac)
 
     train_res_pd = pd.DataFrame(data=np.column_stack(train_res),
                   columns=columns)
 
     test_res_pd = pd.DataFrame(data=np.column_stack(test_res),
                   columns=columns)
-    models = [model_linear, model_sgd, model_xgb, model_gbr, model_theilsen, model_ransac, model_huber]
+    models = [model_linear, model_xgb, model_gbr, model_ransac]
     return train_res_pd, test_res_pd, models
 
 
@@ -204,66 +205,49 @@ train_pd, eval_pd, models = eval_model()
 # In[96]:
 
 def frm(dt):
-    x = 0.1 * dt['lr']  + 0.75 * dt['xgb'] + 0.15 * dt['gbr']
+    x = 0.05 * dt['lr']  + 0.6 * dt['xgb'] + 0.35 * dt['gbr']
     return x
 
 
 # In[97]:
+print("=================================================\n")
 
-print(r2_score(train_y, train_pd.median(axis=1)))
-print(r2_score(eval_y, eval_pd.median(axis=1)))
-print(r2_score(train_y, frm(train_pd)))
-print(r2_score(eval_y, frm(eval_pd)))
+print("Train score : ", r2_score(train_y, train_pd.median(axis=1)))
+print("eval score : ", r2_score(eval_y, eval_pd.median(axis=1)))
+print("Train score : ", r2_score(train_y, frm(train_pd)))
+print("eval score : ", r2_score(eval_y, frm(eval_pd)))
 
 
-# In[98]:
+print("=================================================\n")
 
 all_models = []
 for model in models:
     nme = str(model.__class__.__name__)
     if nme != 'KerasRegressor':
-        model.fit(train_feats_encode, train_labels)
+        model.fit(train_feats_encode, np.log(train_labels))
         print(nme)
-        print(r2_score(train_labels, model.predict(train_feats_encode)))
+        print(r2_score(train_labels, np.exp(model.predict(train_feats_encode))))
         all_models.append(model)
 
-
-# In[36]:
+print("=================================================")
+print("ALL TRAINING DATASET RESULTS")
 
 preds = []
 for model in models:
-    preds.append(model.predict(train_feats_encode))
-columns=['lr', 'sgd', 'xgb', 'gbr',  'thelisen', 'ransac', 'huber']
+    preds.append(np.exp(model.predict(train_feats_encode)))
+columns=['lr', 'sgd', 'xgb', 'gbr', 'ransac', 'huber']
 dt = pd.DataFrame(data=np.array(preds).T, columns=columns)
-r2_score(train_labels, frm(dt))
-
-
-# In[22]:
+print(r2_score(train_labels, frm(dt)))
 
 preds = []
-columns=['lr', 'sgd', 'xgb', 'gbr', 'thelisen', 'ransac', 'huber']
+columns=['lr', 'sgd', 'xgb', 'gbr', 'ransac', 'huber']
 
 for model in models:
     preds.append(model.predict(test_feats_encode))
 
 dt = pd.DataFrame(data=np.array(preds).T, columns=columns)
-
-
-# In[23]:
-
 res = dt[['lr', 'xgb', 'gbr',  'ransac']]
 
-
-# In[24]:
-
 output = pd.DataFrame({'id': test['ID'].astype(np.int32)})
-
-
-# In[29]:
-
 output['y'] = frm(res)
-
-
-# In[30]:
-
 output.to_csv('results/xgboost/ensemble[%s][%s].csv' % ('ensemble','qwe12'), index=False)
